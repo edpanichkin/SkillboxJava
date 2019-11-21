267190bf08e0eb42ecb221561c72bdf6f4922f96
EDPanichkin <edpanichkin@gmail.com>
2019-11-21 17:29:15 +0300

    Final

--- 5/ToDoList/src/Main.java
+++ 5/ToDoList/src/Main.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-        String regex = "(?<cmd>ADD|LIST)\\s*(.*)?";
+        String regex = "(?<cmd>ADD|LIST|DELETE|EDIT)\\s*(?<num>\\d+)?\\s*(?<text>.*)";
         String string = "";
         ArrayList<String> todoList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
